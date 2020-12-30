f044c78949b3ff5e910ef662140fc6df5f8624ec
Ishan Singh <ishan423@gmail.com>
2020-12-29 19:10:04 -0500

    Updates

---
+++ precache-manifest.0cc0c57c523578efad94f472da11a1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7edc14f66044a46f9fcb6efd627fea",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c2d86bd1cbb0fc110eb0",
+    "url": "/todo/static/css/main.419d0f35.chunk.css"
+  },
+  {
+    "revision": "76e1390f88309782007e",
+    "url": "/todo/static/js/2.2b1adc2f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.2b1adc2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d86bd1cbb0fc110eb0",
+    "url": "/todo/static/js/main.2bf760c0.chunk.js"
+  },
+  {
+    "revision": "a03a00bd26cf7edb86b6",
+    "url": "/todo/static/js/runtime-main.8eb62d04.js"
+  }
+]);
